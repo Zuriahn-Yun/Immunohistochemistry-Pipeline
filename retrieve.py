072104d33f3172df3e430a9c6a6b3ed9cdbf9cb5
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2025-07-08 06:32:57 -0700

    This gets blocked

--- retrieve.py
+++ retrieve.py
@@ -4,32 +4,35 @@ import os
 from office365.runtime.auth.authentication_context import AuthenticationContext
 from office365.sharepoint.client_context import ClientContext
 from office365.sharepoint.files.file import File
-from office365.runtime.auth.client_credential import ClientCredential
+from office365.runtime.auth.user_credential import UserCredential
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 try:
-    url = "https://wwu2.sharepoint.com/sites/KaplanLab/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FKaplanLab%2FShared%20Documents%2FIHC%20Cohort%202&newTargetListUrl=%2Fsites%2FKaplanLab%2FShared%20Documents&viewpath=%2Fsites%2FKaplanLab%2FShared%20Documents%2FForms%2FAllItems%2Easpx"
+    url = os.getenv("url")
+    print(url)
     username = os.getenv("username")
     password = os.getenv("password")
+    
       
-    credentials = ClientCredential(username,password)
+    credentials = UserCredential(username,password)
     print("Credentials Loaded")
     ctx = ClientContext(url).with_credentials(credentials)
-    print("Client Context with Credentials Loaded")
+    print("Connected")
     try:
-        web = ctx.web
-        loaded_web = ctx.load(web)
-        ctx.execute_query
-        print("Website Loaded")
-        file_names = []
-        files = ctx.web.get_file_by_server_relative_url(url)
-        print("grabbed files")
-        for files in ctx.files:
-            file_names.append(files)
-        print(file_names)
-    except:
-        print("Error Connecting")
-except:
-    print("Error")
+        folder_url ="/sites/KaplanLab/Shared Documents/IHC Cohort 2"
+        folder = ctx.web.get_folder_by_server_relative_url(folder_url)
+        ctx.load(folder.files)
+        ctx.execute_query()
+        
+        for file in folder.files:
+            print(file.properties["Name"])
+        
+    except Exception as e:
+        print(e)
+except Exception as e:
+    print(e)
     
  
