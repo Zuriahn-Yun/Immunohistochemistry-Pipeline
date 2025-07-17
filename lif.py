8814b603bb17c8440e1c275acf84864660921c05
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2025-07-17 15:26:06 -0700

    lif test file

---
+++ lif.py
@@ -0,0 +1,16 @@
+from analyze import get_rgb
+
+def analyze_lif(file_path):
+    try:
+        print("bello")  
+    except Exception as e:
+        print('Error: ' + e)
+
+
+
+
+def main():
+        url = ""
+
+if __name__ == "__main__":
+    main()
