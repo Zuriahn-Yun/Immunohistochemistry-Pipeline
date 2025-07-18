4c6926223c2b9dfe205c0b4e33a8673d7d919fff
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2025-07-18 10:33:57 -0700

    COMMIT

--- lif.py
+++ lif.py
@@ -1,16 +1,31 @@
 from analyze import get_rgb
+from readlif.reader import LifFile
+import numpy as np
 
 def analyze_lif(file_path):
     try:
-        print("bello")  
+        lif_file = LifFile(file_path)
+        
+        """ Display How many Images """
+        image_count = lif_file
+        print(image_count)
+        
+        
+        lif_image = lif_file.get_image(0)
+        pil_image = lif_file.get_frame(z=0)
+        np_image = np.array(pil_image)
+        np_image.shape
+         
+         
     except Exception as e:
-        print('Error: ' + e)
+        print('Error: ' + str(e))
 
 
 
 
 def main():
-        url = ""
+        url = "IHC Cohort 2 6-4-25.lif"
+        analyze_lif(url)
 
 if __name__ == "__main__":
     main()
