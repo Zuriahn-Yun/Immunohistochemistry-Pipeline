59b20569b31be17f5974dcec973d28d43f1589e6
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2025-07-18 10:41:18 -0700

    imshow is depreciated?

--- lif.py
+++ lif.py
@@ -1,20 +1,29 @@
 from analyze import get_rgb
 from readlif.reader import LifFile
 import numpy as np
+from skimage.io import imshow
 
 def analyze_lif(file_path):
     try:
+        
         lif_file = LifFile(file_path)
         
         """ Display How many Images """
         image_count = lif_file
         print(image_count)
         
-        
+        """ Retrieve a frame from the lif image """
         lif_image = lif_file.get_image(0)
-        pil_image = lif_file.get_frame(z=0)
+        pil_image = lif_image.get_frame(z=0)
+        
+        """ Convert to numpy array """
         np_image = np.array(pil_image)
-        np_image.shape
+        print(np_image.shape)
+        
+        """ Display Image """
+        imshow(np_image)
+        
+        
          
          
     except Exception as e:
