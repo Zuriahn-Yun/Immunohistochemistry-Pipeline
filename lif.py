2f905278849136e4ffae83d911f92b7dfbed3944
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2025-07-21 12:09:06 -0700

    New Functions and analysis

--- lif.py
+++ lif.py
@@ -7,60 +7,24 @@ from PIL import Image
 import time
 from plotly.subplots import make_subplots
 import pandas as pd 
+import os
 
-def analyze_lif(file_path):
-    try:
-        lif_file = LifFile(file_path)
-        
-        """ Display How many Images """
-        image_count = lif_file
-        print(image_count)
-        
-        """ Retrieve a frame from the lif image """
-        lif_image = lif_file.get_image(0)
-        pil_image = lif_image.get_frame(z=0)
-        
-        """ Convert to numpy array """
-        np_image = np.array(pil_image)
-        print(np_image.shape)
-        
-        """ Display Image """
-        fig = px.imshow(np_image)
-        print(fig)
-        fig.show()        
-         
-    except Exception as e:
-        print('Error: ' + str(e))
+def delete_lif(file_path):
+    """ This will delete a lif file, they are typically very large and deleting them can save space to analyze another.
 
+    Args:
+        file_path (string): The local file path.
+    """
+    if os.path.exists(file_path):
+        try:
+            os.remove(file_path)
+            print("File at: " + str(file_path) + " removed successfully.")
+        except Exception as e:
+            print("Error " + str(e))
+    else:
+        print("File not found.")
+            
 def display_lif(file_path,rows,columns):
-    
-    lif = LifFile(file_path)
-    fig = make_subplots(rows,columns)
-    print(lif)
-    curr_row = 1
-    curr_column = 1
-    for image in lif.get_iter_image():
-        print(image)
-        for frame in image.get_iter_t():
-            print(frame)
-            np_image = np.array(frame)
-            curr_px = px.imshow(np_image)
-            trace = curr_px.data[0]
-            fig.add_trace(trace, row=curr_row, col=curr_column)
-            
-            curr_column +=1 
-            if curr_column == columns + 1:
-                curr_row +=1
-                curr_column = 1
-            print(curr_row)
-            print(curr_column)
-            # fig.show()
-            # time.sleep(60)
-            # fig.add_trace(go.Image(z=np_image), row=1, col=2)
-    fig.update_layout(height=512 * rows, width=512 * columns, showlegend=False)
-    fig.show()
-            
-def display(file_path,rows,columns):
     """ This displays a lif file in plotly
 
     Args:
@@ -69,7 +33,7 @@ def display(file_path,rows,columns):
         columns (_type_): _description_
 
     Returns:
-        _type_: _description_
+        np.array : the full lif file in np.array form
     """
     
     # add all the images to one list
@@ -102,11 +66,64 @@ def display(file_path,rows,columns):
     # Return the Dataframe
     return result
 
+def lif_to_np_array(file_path,rows,columns):
+    """ This displays a lif file in plotly
+
+    Args:
+        file_path (_type_): _description_
+        rows (_type_): _description_
+        columns (_type_): _description_
+
+    Returns:
+        np.array : the full lif file in np.array form
+    """
+    
+    # add all the images to one list
+    images = []
+    lif = LifFile(file_path)
+    for image in lif.get_iter_image():
+        print(image)
+        for frame in image.get_iter_t():
+            np_image = np.array(frame)
+            images.append(np_image)
+    
+    # Concat every row and add to a list
+    row_dataframes = []
+
+    for i in range(rows):
+        curr = []
+        for k in range(columns):
+            df = pd.DataFrame(images[(i * columns) + k])
+            curr.append(df)
+        curr_row = pd.concat(curr,axis=1,ignore_index=True)
+        row_dataframes.append(curr_row)
+    
+    # Concat all the rows into one very large dataframe
+    lif_array = pd.concat(row_dataframes, axis=0,ignore_index=True)
+
+    # Return the Dataframe
+    return lif_array
+    
+def analyze_lif(lif_np_array):
+    print(lif_np_array)
+    
+def analyze_lifext(lif_ext_path):
+    # This does not work 
+    # https://pypi.org/project/liffile/
+    print(lif_ext_path)
 
 def main():
-        url = "IHC Cohort 2 6-4-25.lif"
+        lif_other = "IHC Cohort 2 6-4-25.lif"
+        # display_lif(url,8,6)
+        # lif_array = lif_to_np_array(url,8,6)
         
-        display(url,6,8)
+        # analyze_lif(lif_array)
+        
+        lifext = "IHC Cohort 2 5-27-25.lifext"
+        lif = "IHC Cohort 2 5-27-25.lif"
+        
+        extract_ext = LifFile(lif)
+        print("Extracted")
 
 if __name__ == "__main__":
     main()
