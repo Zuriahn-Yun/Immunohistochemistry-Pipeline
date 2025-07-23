0b0d20290553b8c2159a1e2732b671e04745345e
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2025-07-23 13:21:37 -0700

    Added colors

--- test.py
+++ test.py
@@ -43,4 +43,35 @@ for image in lif.get_iter_image():
 print(image_count)
 print(frame_count)
 
+basic_colors = {
+    "black": [0,0,0],
+    "white": [255,255,255],
+    "red": [255,0,0],
+    "lime": [0,255,0],
+    "blue": [0,0,255],
+    "yellow": [255,255,0],
+    "cyan": [0,255,255],
+    "magenta": [255,0,255],
+    "silver": [192,192,192],
+    "gray": [128,128,128],
+    "maroon": [128,0,0],
+    "olive": [128,128,0],
+    "green": [0,128,0],
+    "purple": [128,0,128],
+    "teal": [0,128,128],
+    "navy":[0,0,128],    
+}
 
+import math
+
+test = [250,0,0]
+
+distance_value = 9999999999
+closest_color = "test"
+for key,val in basic_colors.items():
+    curr_distance = math.dist(test,val)
+    if curr_distance < distance_value:
+        distance_value = curr_distance
+        closest_color = key
+        print(distance_value)
+        print(closest_color)
