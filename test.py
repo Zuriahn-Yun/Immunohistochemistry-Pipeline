9173cd3b18750295152d7e0b478cf20d2c27c4b9
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2025-07-23 15:16:09 -0700

    Update File Layout

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ import time
 from PIL import Image
 import readlif
 import extcolors
+import math
+import pandas as pd 
 
 """
 ------------------------------------------------------------------
@@ -17,6 +19,10 @@ LIF File --->
         Image1 --->
             Frame1--->
                 Channel1--->
+                    Z-slices[1] --->
+                    Z-slices[2] --->
+                    ...
+                    Z-slices[75] -->
                 Channel2--->
                 Channel3--->
             Frame2--->
@@ -24,26 +30,8 @@ LIF File --->
 ------------------------------------------------------------------
 """
 
-url = "IHC Cohort 2 6-4-25.lif"
-# In this data I only have one frame per image 
-lif = LifFile(url)
-image_count = 0
-frame_count = 0
-for image in lif.get_iter_image():
-    image_count +=1
-    for frame in image.get_iter_t():
-        frame_count +=1
-        img = Image.fromarray(np.array(frame))
-        colors,pixel_count = extcolors.extract_from_image(img)
-        print(colors)
-        
-        break
-    break
-        
-print(image_count)
-print(frame_count)
-
-basic_colors = {
+def closest_color(rgb):
+    basic_colors = {
     "black": [0,0,0],
     "white": [255,255,255],
     "red": [255,0,0],
@@ -60,18 +48,39 @@ basic_colors = {
     "purple": [128,0,128],
     "teal": [0,128,128],
     "navy":[0,0,128],    
-}
+    }
+    
+    distance_value = 9999999999
+    closest_color = "test"
+    print(rgb)
+    for key,val in basic_colors.items():
+        curr_distance = math.dist(rgb,val)
+        if curr_distance < distance_value:
+            distance_value = curr_distance
+            closest_color = key
+    return closest_color
 
-import math
-
-test = [250,0,0]
-
-distance_value = 9999999999
-closest_color = "test"
-for key,val in basic_colors.items():
-    curr_distance = math.dist(test,val)
-    if curr_distance < distance_value:
-        distance_value = curr_distance
-        closest_color = key
-        print(distance_value)
-        print(closest_color)
+url = "IHC Cohort 2 6-4-25.lif"
+# In this data I only have one frame per image 
+lif = LifFile(url)
+image_count = 0
+frame_count = 0
+colors = []
+for image in lif.get_iter_image():
+    image_count +=1
+    print(image.dims)
+    curr = image.get_frame()
+    for frame in image.get_iter_t():
+        frame_count +=1
+        for i in range(3):
+            frame = image.get_frame(c=i)
+        frame = image.get_frame(c=2)
+        img_array = np.array(frame)
+        print("IMAGE ARRAY")
+        print(img_array)
+        print(img_array.shape)
+        for row in img_array:
+            color = closest_color(rgb=row)
+            colors.append(color)
+    print(colors)
+    exit()
